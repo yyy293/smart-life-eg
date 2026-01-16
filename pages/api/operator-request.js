392b82b133ce5a4bf1cf37d753c860175b4cfb2f
yyy293 <yassein.ko10@gmail.com>
2026-01-16 16:48:24 +0200

    Update operator-request.js

--- pages/api/operator-request.js
+++ pages/api/operator-request.js
@@ -1,7 +1,8 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ message: "Method not allowed" });
+  if (req.method !== "POST")
+    return res.status(405).json({ message: "Method not allowed" });
 
   const { email } = req.body;
   if (!email) return res.status(400).json({ message: "Email is required" });
